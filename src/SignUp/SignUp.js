c6cceb62b8f13075b785f8d2fbd981c5ae8f5788
sobhy <Admin@SobhyDaaboul>
2025-01-02 21:40:13 +0200

    signuppage

---
+++ src/SignUp/SignUp.js
@@ -0,0 +1,38 @@
+import './SignUp.css';
+
+
+function SignUp(){
+  return (
+    <div class="mainclass">
+      <form class="formclass">
+      <div className="nameContainer">
+          <div className="firstName">
+            <label>First Name</label>
+            <input type="text" placeholder="Enter your first name" />
+          </div>
+          <div className="lastName">
+            <label>Last Name</label>
+            <input type="text" placeholder="Enter your last name" />
+          </div>
+      </div>
+        <div class="Number"> 
+          <label>Phone Number</label>
+          <input type="text" placeholder="your PhoneNumber"/>
+        </div>
+        <div class="Email">
+          <label>Email</label>
+          <input type="text" placeholder="test@example.com"/>
+        </div>
+        <div class="Password">
+          <label>Password</label>
+          <input type="text" placeholder="yourpassword"/>
+        </div>
+        <div>
+          <button class="Submit-btn">SignUp</button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default SignUp;
--- src/index.js
+++ src/index.js
@@ -1,2 +1,14 @@
-import App from './App';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import SignUp from './SignUp/SignUp';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    
+    <SignUp />
+  </React.StrictMode>
+);
+
 
