d776978d0e215052b5908187f230291d75813a33
Charbel178 <charbelrizk7171@gmail.com>
2025-01-05 00:37:13 +0200

    fixing

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -4,14 +4,8 @@ import Footerimage from "../assets/images/Footerimage.jpg";
 function Footer() {
     return (
         <footer className={classes.footer}>
-            <div className={classes.quotes}>
-                <p>"Life Doesn't Become Easier, You Become Stronger.”</p>
-                <p>"Dedication Has No Limitation."</p>
-                <p>Beirut, Lebanon</p>
-                <p>EST.2025</p>
-            </div>
-            <div className={classes.logo}>
-                <img src={Footerimage} alt='Gym Logo'/>
+            <div className={classes.footerimage}>
+                <img src={Footerimage} alt="Gym Footer"/>
             </div>
         </footer>
     )
