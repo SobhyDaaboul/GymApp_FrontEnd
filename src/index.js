4ea5dd220a9fd33fd454cf673cf1849298f35389
Charbel178 <charbelrizk7171@gmail.com>
2025-01-05 00:34:07 +0200

    nothing

--- src/index.js
+++ src/index.js
@@ -1,19 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {BrowserRouter} from 'react-router-dom';
-<<<<<<< HEAD
-import MainPage from './layout/MainPage';
-=======
-import Footer from './HomePage/Footer';
->>>>>>> 2d225a8fd66fb292e05709da78314a51039b15c1
+import Footer from './layout/Footer';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-<<<<<<< HEAD
-    <MainPage/>
-=======
     <Footer />
->>>>>>> 2d225a8fd66fb292e05709da78314a51039b15c1
   </BrowserRouter>
 );
