bc1332a088a9742e028f3f3e5ace2bb1c526b313
sobhy <Admin@SobhyDaaboul>
2025-01-05 02:27:00 +0200

    issues

--- src/layout/MainPage.js
+++ src/layout/MainPage.js
@@ -2,16 +2,28 @@ import React from "react";
 import AutoCarousel from "../components/AutoCarousel";
 import Carousel from '../assets/images/Carousel.png';
 import Carousel1 from '../assets/images/Carousel1.png';
+import classes from './MainPage.module.css';
 
 const imageUrls=[Carousel,Carousel1];
 
 
 function MainPage(){
   return(
-    <div>
-      <AutoCarousel images={imageUrls}/>
+    <div className={classes.mainclass}>
+      <div>
+        <AutoCarousel images={imageUrls}/>
+      </div>
+
+      <div>
+        
+      </div>
+
+      <div>
+        
+      </div>
+  
     </div>
-  )
+  );
 }
 
 export default MainPage;
