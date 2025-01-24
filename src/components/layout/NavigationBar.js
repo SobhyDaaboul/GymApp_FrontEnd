4a3f2c29d12d39aad8c65aad66629e23c5158fd1
sobhy <Admin@SobhyDaaboul>
2025-01-24 14:12:32 +0200

    adding errors

--- src/components/layout/NavigationBar.js
+++ src/components/layout/NavigationBar.js
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import Logo from "../../assets/icons/Logo.png";
+import Logo from "../../assets/icons/Logosmall.png";
 import classes from "../../CSS/NavigationBar.module.css";
 
 function NavigationBar() {
