456ab086a9c0151c8b7e6762e656874f4206bbbd
sobhy <Admin@SobhyDaaboul>
2025-01-24 15:44:27 +0200

    changing navbar

--- src/components/layout/NavigationBar.js
+++ src/components/layout/NavigationBar.js
@@ -1,41 +1,39 @@
-import { Link } from "react-router-dom";
-import Logo from "../../assets/icons/Logosmall.png";
 import classes from "../../CSS/NavigationBar.module.css";
+import Logo from "../../assets/icons/Logosmall.png";
+import { Link } from "react-router-dom";
 
 function NavigationBar() {
   return (
-    <header className={classes.header}>
-      <div className={classes.logo}>
-        <Link to="/home-page">
-          <img src={Logo} alt="Gym Logo" />
-        </Link>
-      </div>
-      <nav>
-        <ul className={classes.navList}>
-          <li className={classes.navItem}>
-            <Link className={classes.navLink} to="/create-membership">
-              Create Membership
-            </Link>
-          </li>
-          <li className={classes.navItem}>
-            <Link className={classes.navLink} to="/Classes">
-              Classes
-            </Link>
-          </li>
-          <li className={classes.navItem}>
-            <Link className={classes.navLink} to="/MyWorkout">
-              MyWorkout
-            </Link>
-          </li>
-          <li className={classes.navItem}>
-            <Link className={classes.navLink} to="/PtSessions">
-              PtSessions
-            </Link>
-          </li>
-        </ul>
-        <button className={classes.NavSignUp}>SignUp</button>
-      </nav>
-    </header>
+    <nav className={classes.navbar}>
+      <Link className={classes.logo} to="/home-page">
+        <img src={Logo} alt="Gym Logo" />
+      </Link>
+      <ul className={classes.navList}>
+        <li className={classes.navItem}>
+          <Link className={classes.navLink} to="/create-membership">
+            Create Membership
+          </Link>
+        </li>
+        <li className={classes.navItem}>
+          <Link className={classes.navLink} to="/Classes">
+            Classes
+          </Link>
+        </li>
+        <li className={classes.navItem}>
+          <Link className={classes.navLink} to="/MyWorkout">
+            MyWorkout
+          </Link>
+        </li>
+        <li className={classes.navItem}>
+          <Link className={classes.navLink} to="/PtSessions">
+            PtSessions
+          </Link>
+        </li>
+      </ul>
+      <Link to="/SignUp">
+        <button className={classes.NavSignUpbtn}>SignUp</button>
+      </Link>
+    </nav>
   );
 }
 
